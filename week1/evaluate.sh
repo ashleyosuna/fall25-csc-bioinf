5f01f9b01a706213c65f6ad6d65fc11d1e8196d7
Ashley Osuna <ashleyosuna@uvic.ca>
2025-09-14 10:41:13 -0700

    worked on script, outputting formatted information

---
+++ week1/evaluate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+data="data1 data2"
+
+printf "%-10s %-10s %-10s %-10s\n" "Dataset" "Language" "Runtime" "N50"
+echo "---------------------------------------------------------"
+
+for d in $data; do
+    # python
+    start=$(date +%s)
+    python3 ./genome-assembly-repo/main.py ./data/$d > ./tests/lengths_python_$d.txt
+    end=$(date +%s)
+    elapsed=$(( end - start ))
+    
+    formatted=$(printf "%d:%02d:00" $((elapsed/3600)) $((elapsed%3600)))
+    
+    n50_python=$(codon run -release ./tests/n50.py ./tests/lengths_python_$d.txt)
+
+    printf "%-10s %-10s %-10s %-10d\n" ${d} "python" ${formatted} ${n50_python}
+
+    
+    # codon
+    start=$(date +%s)
+    codon run -release ./code/main.py ./data/$d > ./tests/lengths_codon_$d.txt
+    end=$(date +%s)
+
+    elapsed=$((end - start))
+    formatted=$(printf "%d:%02d:00" $((elapsed/3600)) $((elapsed%3600)))
+
+    n50_codon=$(codon run -release ./tests/n50.py ./tests/lengths_codon_$d.txt)
+    printf "%-10s %-10s %-10s %-10d\n" ${d} "codon" ${formatted} ${n50_codon}
+done
