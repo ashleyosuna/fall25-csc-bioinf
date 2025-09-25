f743774a4c8a0c1425d2d0c65efa6d84274d959c
Michelle Rosenthal <127794503+mtrinhsenthal@users.noreply.github.com>
2025-09-25 16:53:29 -0700

    Add gc_content property to pssm

--- week2/code/matrix.py
+++ week2/code/matrix.py
@@ -152,37 +152,6 @@ class GenericPositionMatrix:
         return Seq.Seq(sequence)
 
     def calculate_consensus(self, substitution_matrix=None, plurality=None, identity=0, setcase=None):
-        """Return the consensus sequence (as a string) for the given parameters.
-
-        This function largely follows the conventions of the EMBOSS `cons` tool.
-
-        Arguments:
-         - substitution_matrix - the scoring matrix used when comparing
-           sequences. By default, it is None, in which case we simply count the
-           frequency of each letter.
-           Instead of the default value, you can use the substitution matrices
-           available in Bio.Align.substitution_matrices. Common choices are
-           BLOSUM62 (also known as EBLOSUM62) for protein, and NUC.4.4 (also
-           known as EDNAFULL) for nucleotides. NOTE: This has not yet been
-           implemented.
-         - plurality           - threshold value for the number of positive
-           matches, divided by the total count in a column, required to reach
-           consensus. If substitution_matrix is None, then this argument must
-           be None, and is ignored; a ValueError is raised otherwise. If
-           substitution_matrix is not None, then the default value of the
-           plurality is 0.5.
-         - identity            - number of identities, divided by the total
-           count in a column, required to define a consensus value. If the
-           number of identities is less than identity * total count in a column,
-           then the undefined character ('N' for nucleotides and 'X' for amino
-           acid sequences) is used in the consensus sequence. If identity is
-           1.0, then only columns of identical letters contribute to the
-           consensus. Default value is zero.
-         - setcase             - threshold for the positive matches, divided by
-           the total count in a column, above which the consensus is in
-           upper-case and below which the consensus is in lower-case. By
-           default, this is equal to 0.5.
-        """
         alphabet = self.alphabet
         if set(alphabet).union(set("ACGTUN-")) == set("ACGTUN-"):
             undefined = "N"
@@ -410,6 +379,11 @@ class PositionSpecificScoringMatrix(GenericPositionMatrix):
             score += min(self[letter][position] for letter in letters)
         return score
 
+    @property
+    def gc_content(self):
+        """Compute the GC-ratio."""
+        return super().gc_content
+
 values: Dict[str, List[int]] = {"A": [1, 2, 3], "C": [2, 3, 4], "G": [1, 2, 3], "T": [2, 4, 5]}
 # positionMatrix = GenericPositionMatrix(alphabet="ACGT", values=values)
 # print(positionMatrix)
@@ -436,4 +410,4 @@ positionSpecific = PositionSpecificScoringMatrix(alphabet="ACGT", values=values)
 print(positionSpecific)
 # print(positionSpecific.max)
 # print(positionSpecific.min)
-# print(positionSpecific.mean())
+# print(positionSpecific.gc_content)
