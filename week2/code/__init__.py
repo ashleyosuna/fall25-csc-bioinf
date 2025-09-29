964d3a96c5a8505170a069738bf820b8d26b09f9
Ashley Osuna <ashleyosuna@uvic.ca>
2025-09-29 09:14:48 -0700

    background and pseudocounts for motif class

--- week2/code/__init__.py
+++ week2/code/__init__.py
@@ -15,8 +15,10 @@ class Motif:
     length: Optional[int]
     alignment: Optional[pyobj]
     alphabet: str
-    pseudocounts: Optional[int]
-    # background: Optional[]
+    _pseudocounts: Optional[Dict[str, float]]
+    # background: Optional[Dict[str, float]]
+    _background: Optional[Dict[str, float]]
+
     # mask: Optional[]
 
     def __init__(self, alphabet="ACGT", alignment: Optional[pyobj]=None, counts: Optional[Dict[str, List[float]]]=None):
@@ -44,8 +46,9 @@ class Motif:
             self.alignment = None
             self.length = None
         self.alphabet = alphabet
-        self.pseudocounts = None
-        # self.background = None
+        self._pseudocounts = dict.fromkeys(self.alphabet, 0.0)
+        self._background = None
+        self.__set_background(value=None)
         # self.mask = None
     
     def __len__(self):
@@ -76,54 +79,56 @@ class Motif:
     def format(self, format_spec):
         return self.__format__(format_spec)
     
-    # def __get_background(self):
-    #     return self._background
+    @property
+    def pseudocounts(self):
+        return self._pseudocounts
     
-    # def __set_background(self, value: dict[str, float]):
-    #     self._background = {letter: value[letter] for letter in self.alphabet}
-    #     total = sum(self._background.values())
-    #     for letter in self.alphabet:
-    #         self._background[letter] /= total
+    @pseudocounts.setter
+    def pseudocounts(self, value):
+        self._pseudocounts = {}
+        if isinstance(value, Dict[str, float]):
+            self._pseudocounts = {letter: value[letter] for letter in self.alphabet}
+        else:
+            if value is None:
+                value = 0.0
+            self._pseudocounts = dict.fromkeys(self.alphabet, value)
     
-    # def __set_background(self, value: NoneType = None):
-    #     self._background = dict.fromkeys(self.alphabet, 1.0)
-    #     total = sum(self._background.values())
-    #     for letter in self.alphabet:
-    #         self._background[letter] /= total
+    @property
+    def background(self):
+        return self._background
     
-    # def __set_background(self, value = float):
-    #     T_or_U = "T" if self._has_dna_alphabet() else "U"
-    #     self._background["A"] = (1.0 - value) / 2.0
-    #     self._background["C"] = value / 2.0
-    #     self._background["G"] = value / 2.0
-    #     self._background[T_or_U] = (1.0 - value) / 2.0
-    #     total = sum(self._background.values())
-    #     for letter in self.alphabet:
-    #         self._background[letter] /= total
+    def __set_background(self, value = None):
+        if isinstance(value, Dict[str, float]): self._background = {letter: value[letter] for letter in self.alphabet}
+        elif value is None: self._background = dict.fromkeys(self.alphabet, 1.0)
+        elif isinstance(value, float) or isinstance(value, int):
+            if not self._has_dna_alphabet() and not self._has_rna_alphabet():
+                raise ValueError(
+                    "Setting the background to a single value only works for DNA and RNA"
+                    "motifs (in which case the value is interpreted as the GC content)"
+                )
+            value = float(value)
+            T_or_U = "T" if self._has_dna_alphabet() else "U"
+            d = {}
+            d['A'] = (1.0 - value) / 2.0
+            d["C"] = value / 2.0
+            d["G"] = value / 2.0
+            d[T_or_U] = (1.0 - value) / 2.0
+            self._background = d
+        
+        # normalize
+        total = sum(self._background.values())
+        for letter in self.alphabet:
+            self._background[letter] /= total
+    
+    @background.setter
+    def background(self, value = None):
+        self.__set_background(value)
 
-    # def __set_background(self, value):
-    #     if isinstance(value, dict):
-    #         self._background = {letter: value[letter] for letter in self.alphabet}
-    #     elif value is None:
-    #         self._background = dict.fromkeys(self.alphabet, 1.0)
-    #     else:
-    #         if not self._has_dna_alphabet() and not self._has_rna_alphabet():
-    #             raise ValueError(
-    #                 "Setting the background to a single value only works for DNA and RNA"
-    #                 "motifs (in which case the value is interpreted as the GC content)"
-    #             )
-    #         T_or_U = "T" if self._has_dna_alphabet() else "U"
-    #         self._background["A"] = (1.0 - value) / 2.0
-    #         self._background["C"] = value / 2.0
-    #         self._background["G"] = value / 2.0
-    #         self._background[T_or_U] = (1.0 - value) / 2.0
-    #     total = sum(self._background.values())
-    #     for letter in self.alphabet:
-    #         self._background[letter] /= total
-
-    # background = property(__get_background, __set_background)
-    # del __get_background
-    # del __set_background
+    def _has_dna_alphabet(self):
+        return sorted(self.alphabet) == ["A", "C", "G", "T"]
+    
+    def _has_rna_alphabet(self):
+        return sorted(self.alphabet) == ["A", "C", "G", "U"]
     
 # motif = Motif(counts={'A': [1.0, 2.0, 3.0], 'C': [2.0, 3.0, 4.0], 'G': [1.0, 2.0, 3.0], 'T': [2.0, 3.0, 4.0]})
 # print('-->', len(motif))
