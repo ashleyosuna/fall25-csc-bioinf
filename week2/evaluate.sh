66379e0de5c889a91169a17a945f5c33f472ac95
Michelle Rosenthal <127794503+mtrinhsenthal@users.noreply.github.com>
2025-09-30 12:03:53 -0700

    codon path

--- week2/evaluate.sh
+++ week2/evaluate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
-PATH=${PATH}:${HOME}/.codon/bin
+# PATH=${PATH}:${HOME}/.codon/bin
+export PATH="${HOME}/.codon/bin:$PATH"
 echo "week2"
 tests_motif=$(codon run week2/code/tests_motif.py)
 echo $tests_motif
