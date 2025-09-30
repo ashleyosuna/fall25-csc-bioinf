a2e22de13f871d1db69076ff3405c6d95d9eeb2b
Michelle Rosenthal <127794503+mtrinhsenthal@users.noreply.github.com>
2025-09-30 12:09:46 -0700

    trying to get libpython.so

--- week2/evaluate.sh
+++ week2/evaluate.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
-# PATH=${PATH}:${HOME}/.codon/bin
-export PATH="${HOME}/.codon/bin:$PATH"
+PATH=${PATH}:${HOME}/.codon/bin
 echo "week2"
 tests_motif=$(codon run week2/code/tests_motif.py)
 echo $tests_motif
