129b790e5054ddd5841ec4e5e2a7538ad4388df2
Michelle Rosenthal <127794503+mtrinhsenthal@users.noreply.github.com>
2025-09-30 13:06:00 -0700

    try python first

--- week2/evaluate.sh
+++ week2/evaluate.sh
@@ -2,9 +2,9 @@
 set -euxo pipefail
 PATH=${PATH}:${HOME}/.codon/bin
 echo "week2"
-echo "codon"
-tests_motif=$(codon run week2/code/tests_motif.py)
-echo $tests_motif
 echo "python"
 tests_motif=$(python week2/code/tests_motif.py)
+echo $tests_motif
+echo "codon"
+tests_motif=$(codon run week2/code/tests_motif.py)
 echo $tests_motif
