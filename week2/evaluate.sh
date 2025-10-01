c5f5dd2e3d52c7e39189a114a530657daa417b8e
Michelle Rosenthal <127794503+mtrinhsenthal@users.noreply.github.com>
2025-10-01 12:59:52 -0700

    fixed typo in evaluate.sh

--- week2/evaluate.sh
+++ week2/evaluate.sh
@@ -5,4 +5,4 @@ echo "week2"
 echo "running Codon tests"
 tests_motif=$(codon run week2/code/tests_motif.py)
 echo "running Python tests"
-tests_motif=$(python3 run week2/code/tests_motif.py)
+tests_motif=$(python3 run week2/code/tests_python.py)
