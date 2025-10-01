d7d4c0adb0c3e0ca54c4a2a1ba4b3bb25fa0dda7
Michelle Rosenthal <127794503+mtrinhsenthal@users.noreply.github.com>
2025-10-01 13:24:00 -0700

    add release flag to codon

--- week2/evaluate.sh
+++ week2/evaluate.sh
@@ -3,6 +3,6 @@ set -euxo pipefail
 PATH=${PATH}:${HOME}/.codon/bin
 echo "week2"
 echo "running Codon tests"
-tests_motif=$(codon run week2/code/tests_motif.py)
+tests_motif=$(codon run -release week2/code/tests_motif.py)
 echo "running Python tests"
 tests_motif=$(python3 week2/code/tests_python.py)
