660a61f5bef1b886b82437db16519ee11bee5fda
Ashley Osuna <ashleyosuna@uvic.ca>
2025-10-01 14:01:46 -0700

    formatting

--- week2/evaluate.sh
+++ week2/evaluate.sh
@@ -6,5 +6,6 @@ echo "week2"
 echo -e "\nRunning codon tests\n"
 codon run -release week2/code/tests_motif.py
 
+echo "----------------------------"
 echo -e "\nRunning python tests\n"
 python3 week2/code/tests_python.py
