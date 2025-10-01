8dda9aa3e515e1bef935427f6ca98522707896f3
Ashley Osuna <ashleyosuna@uvic.ca>
2025-10-01 13:50:39 -0700

    printing which tests are running

--- week2/evaluate.sh
+++ week2/evaluate.sh
@@ -2,5 +2,9 @@
 set -euxo pipefail
 PATH=${PATH}:${HOME}/.codon/bin
 echo "week2"
+
+echo "\nRunning codon tests\n"
 codon run -release week2/code/tests_motif.py
+
+echo "\nRunning python tests\n"
 python3 week2/code/tests_python.py
