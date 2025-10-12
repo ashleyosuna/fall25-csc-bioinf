3ca9a5247b340a7dd6a6993d0ed62519ae494577
Ashley Osuna <ashleyosuna@uvic.ca>
2025-10-12 11:00:37 -0700

    printing time elapsed

--- week3/evaluate.sh
+++ week3/evaluate.sh
@@ -3,8 +3,17 @@
 PATH=${PATH}:${HOME}/.codon/bin
 echo "week3"
 
-echo -e "\nRunning codon tests\n"
-codon run -release week3/code/test_codon.py
+printf "%-10s %-10s\n" "Language" "Runtime"
+echo "----------------------------"
+
+start=$(date +%s%3N)
+# echo -e "\nRunning codon tests\n"
+tmp=codon run -release week3/code/test_codon.py
+
+end_time=$(date +%s%3N)
+
+duration_ms=$((end_time - start_time))
+printf "%-10s %-10f\n" "codon" ${duration_ms}
 
 echo "----------------------------"
 echo -e "\nRunning python tests\n"
