047c96307acb8373c1834d9e6cd9346e144932b6
Ashley Osuna <ashleyosuna@uvic.ca>
2025-10-12 11:30:09 -0700

    formatting?

--- week3/evaluate.sh
+++ week3/evaluate.sh
@@ -6,7 +6,7 @@ echo "week3"
 printf "%-10s %-10s\n" "Language" "Runtime"
 echo "----------------------------"
 time=$(codon run -release week3/code/test_codon.py)
-printf "%-10s %-10s\n" "Codon" ${time}
+printf "%-10s %-10s %-2s\n" "Codon" ${time}
 
 time=$(python3 week3/code/test_python.py)
-printf "%-10s %-10s\n" "Python" ${time}
+printf "%-10s %-10s %-2s\n" "Python" ${time}
