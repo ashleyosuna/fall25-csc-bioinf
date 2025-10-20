b01d4cc17a2023c008dd44d08b46bbdc934bde89
Ashley Osuna <ashleyosuna@uvic.ca>
2025-10-20 12:34:33 -0700

    global, local, and affine alignment in python

---
+++ week4/code/python/affine_alignment.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+MIN = float('-inf')
+
+def affine_alignment(u, v, match_score = 3, mismatch_score = -3, gap_init_score = -5, gap_extension_score = -3):
+    n, m = len(u) + 1, len(v) + 1
+
+    lower = np.ndarray((n, m))
+    middle = np.ndarray((n, m))
+    upper = np.ndarray((n, m))
+
+    lower[0][0] = MIN
+    middle[0][0] = 0
+    upper[0][0] = MIN
+    # initialize first column
+    for i in range(1, n): lower[i][0] = (gap_init_score if i == 1 else lower[i-1][0] + gap_extension_score)
+
+    # initialize first row
+    for j in range(1, m): upper[0][j] = (gap_init_score if j == 1 else upper[0][j-1] + gap_extension_score)
+
+    # construct matrices
+    for i in range(1, n):
+        for j in range(1, m):
+            # lower
+            lower[i][j] = max(
+                lower[i-1][j] + gap_extension_score,
+                middle[i-1][j] + gap_init_score
+            )
+
+            upper[i][j] = max(
+                upper[i][j-1] + gap_extension_score,
+                middle[i][j-1] + gap_init_score
+            )
+
+            middle[i][j] = max(
+                lower[i][j],
+                middle[i-1][j-1] + (match_score if u[i-1] == v[j-1] else mismatch_score),
+                upper[i][j]
+            )
+
+    # backtrace
+    i, j = n-1, m-1
+    alignment = [[], []]
+
+    while i > 0 or j > 0:
+        if middle[i][j] == middle[i-1][j-1] + mismatch_score \
+            or u[i-1] == v[j-1]:
+            i -= 1
+            j -= 1
+            alignment[0].append(u[i])
+            alignment[1].append(v[j])
+        # vertical gap
+        elif middle[i][j] == lower[i][j]:
+            i -= 1
+            alignment[0].append(u[i])
+            alignment[1].append("-")
+        else:
+            j -= 1
+            alignment[0].append("-")
+            alignment[1].append(v[j])
+    
+    alignment[0].reverse()
+    alignment[1].reverse()
+    return alignment
+
+al = affine_alignment("GATCCAG", "GACAG")
+print(al[0], al[1], sep="\n")
--- week4/code/python/local_alignment.py
+++ week4/code/python/local_alignment.py
@@ -1,15 +1,13 @@
 import numpy as np
 
-def global_alignment(u, v, match_score = 3, mismatch_score = -3, gap_score = -2):
+def local_alignment(u, v, match_score = 3, mismatch_score = -3, gap_score = -2):
     # CONSTRUCT MATRIX
     n, m = len(u) + 1, len(v) + 1
     network = np.ndarray((n, m))
 
-    # fill first row
-    for i in range(n): network[i][0] = -2 * i
-
-    # fill first column
-    for j in range(m): network[0][j] = -2 * j
+    # keep track of where best local alignment ends so we can backtrack later
+    max_score = 0
+    max_score_position = (0, 0)
 
     # fill matrix row by row
     for i in range(1, n):
@@ -17,35 +15,40 @@ def global_alignment(u, v, match_score = 3, mismatch_score = -3, gap_score = -2)
             network[i][j] = max(
                 network[i-1][j] + gap_score,
                 network[i][j-1] + gap_score,
-                network[i-1][j-1] + (match_score if u[i-1] == v[j-1] else mismatch_score)
+                network[i-1][j-1] + (match_score if u[i-1] == v[j-1] else mismatch_score),
+                0
             )
+
+            if network[i][j] > max_score:
+                max_score = network[i][j]
+                max_score_position = (i, j)
 
     # BACKTRACK TO CONSTRUCT ALIGNMENT
     alignment = [[], []]
-    i, j = n - 1, m - 1
+    i, j = max_score_position
 
     while i > 0 or j > 0:
-        # if match or mismatch, 'move' along both sequences
         if network[i][j] == network[i-1][j-1] + mismatch_score or \
             u[i-1] == v[j-1]:
             i -= 1
             j -= 1
             alignment[0].append(u[i])
             alignment[1].append(v[j])
-        # vertical gap, 'move' along u
         elif network[i][j] == network[i-1][j] + gap_score:
             i -= 1
             alignment[0].append(u[i])
             alignment[1].append('-')
-        # horizontal gap, 'move' along v
-        else:
+        elif network[i][j] == network[i][j-1] + gap_score:
             j -= 1
             alignment[0].append("-")
             alignment[1].append(v[j])
+        # current position is where local alignment starts
+        else:
+            break
         
     alignment[0].reverse()
     alignment[1].reverse()
     return alignment
 
-al = global_alignment("TACGAA", "TATGA")
-print(al[0], al[1], sep="\n")
+al = local_alignment("AAGTG", "GGCTG")
+print(al[0], al[1], sep='\n')
