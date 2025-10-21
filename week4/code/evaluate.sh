91975d70d4980d31f9a3f0740a2758f65b66ab48
Ashley Osuna <ashleyosuna@uvic.ca>
2025-10-21 08:34:17 -0700

    did not commit evaluate before lol

---
+++ week4/code/evaluate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euxo pipefail
+PATH=${PATH}:${HOME}/.codon/bin
+echo "week3"
+
+printf "%-16s %-16s %-16s\n" "Method" "Language" "Runtime"
+echo "----------------------------"
+# time=$(codon run -release week3/code/test_codon.py)
+# printf "%-10s %-10s %-2s\n" "Codon" ${time}
+
+# time=$(python3 week3/code/test_python.py)
+# printf "%-10s %-10s %-2s\n" "Python" ${time}
+python3 wee4/code/python/tests.py
